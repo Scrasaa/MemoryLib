c745981e2c75a03ea071b3884a9a6286d20133a0
Your Name <you@example.com>
2022-04-19 15:21:11 +0200

    Added PatternScan, only internally. Included ethe ntdll header for PatternScan. Made 2 more classes!!!

---
+++ MemoryLib/MemoryLib.h
@@ -0,0 +1,60 @@
+#pragma once
+
+#include "pch.h"
+#include "framework.h"
+#include "ntdll.h"
+
+class CHook
+{
+private:
+    bool Hook32(uintptr_t pHookStart, uintptr_t pOurFunction, size_t iLength);
+public:
+    bool Detour32(uintptr_t pHookStart, uintptr_t pOurFunction, size_t iLength);
+};
+
+class CPatternScan
+{
+private:
+    char* ScanWrapper(char* pattern, char* mask, char* begin, size_t size);
+
+    void Parse(char* combo, char* pattern, char* mask);
+
+    char* PatternScan(char* pattern, char* mask, char* begin, size_t size);
+
+    char* TO_CHAR(wchar_t* string);
+
+    PEB* GetPEB();
+
+    LDR_DATA_TABLE_ENTRY* GetLDREntry(std::string name);
+
+public:
+    intptr_t PatternScanInternal(char* combopattern, std::string modName);
+};
+
+class CMemory : public CPatternScan, public CHook
+{
+private:
+    uintptr_t procID = NULL;
+    HANDLE hHandle = NULL;
+
+    ~CMemory();
+public:
+    uintptr_t GetModuleBaseAddress(const char* szModuleName);
+
+    uintptr_t GetProcessID(const char* szProcessName);
+
+    template <typename value>
+    uintptr_t ReadMem(uintptr_t addy);
+
+    template <typename value>
+    void WriteMem(uintptr_t addy, value val);
+
+    void InNop(LPVOID dst, size_t iSize);
+
+    void InPatch(LPVOID dst, LPVOID src, size_t iSize);
+
+    void ExPatch(LPVOID dst, LPVOID src, size_t iSize);
+
+    void ExNop(LPVOID dst, size_t iSize);
+
+};
