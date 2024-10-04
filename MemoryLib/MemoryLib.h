6ce84212f182c255fe35da8bb9ead3fe4c2a2318
Scrasaa <78956958+Scrasaa@users.noreply.github.com>
2024-10-04 05:34:44 +0200

    Update MemoryLib.h

--- MemoryLib/MemoryLib.h
+++ MemoryLib/MemoryLib.h
@@ -12,94 +12,92 @@
 class CMemory
 {
 private:
-    DWORD GetHashFromString(char* szString) const;
+	DWORD GetHashFromString(char* szString) const;
 
-    LPCWSTR ConvertToLPCWSTR(const char* szModuleName) const;
+	LPCWSTR ConvertToLPCWSTR(const char* szModuleName) const;
 
-    int CmpUnicodeStr(const WCHAR* substr, const WCHAR* mystr);
+	int CmpUnicodeStr(const WCHAR* substr, const WCHAR* mystr);
 protected:
-    PEB* GetPEB() const;
+	PEB* GetPEB() const;
 public:
-    HMODULE ResolveModuleBaseAddressPEB(char* szModuleName) const;
+	HMODULE ResolveModuleBaseAddressPEB(char* szModuleName) const;
 
-    uintptr_t GetModuleBaseAddress(const char* szModuleName, uintptr_t procID);
+	uintptr_t GetModuleBaseAddress(const char* szModuleName, uintptr_t procID);
 
-    uintptr_t GetProcessID(const char* szProcessName);
+	uintptr_t GetProcessID(const char* szProcessName);
 
-    template <typename value>
-    uintptr_t ReadMem(uintptr_t addy, HANDLE hProcess);
+	template <typename value>
+	uintptr_t ReadMem(uintptr_t addy, HANDLE hProcess);
 
-    template <typename value>
-    void WriteMem(uintptr_t addy, value val, HANDLE hProcess);
+	template <typename value>
+	void WriteMem(uintptr_t addy, value val, HANDLE hProcess);
 
-    void InNop(LPVOID dst, size_t iSize);
+	void InNop(LPVOID dst, size_t iSize);
 
-    void InPatch(LPVOID dst, LPVOID src, size_t iSize);
+	void InPatch(LPVOID dst, LPVOID src, size_t iSize);
 
-    void ExPatch(LPVOID dst, LPVOID src, size_t iSize, HANDLE hProcess);
+	void ExPatch(LPVOID dst, LPVOID src, size_t iSize, HANDLE hProcess);
 
-    void ExNop(LPVOID dst, size_t iSize, HANDLE hProcess);
+	void ExNop(LPVOID dst, size_t iSize, HANDLE hProcess);
 
-    HANDLE GetProcess(uintptr_t procID);
+	HANDLE GetProcess(uintptr_t procID);
 
-    MODULEENTRY32 GetModuleEntry(const char* szModuleName, uintptr_t procID);
-    
-    PDWORD GetFunctionAddressByHash(char* library, DWORD hash) const;
+	MODULEENTRY32 GetModuleEntry(const char* szModuleName, uintptr_t procID);
 
-    void* GetFunctionAddress(char* MyNtdllFunction, PVOID MyDLLBaseAddress) const;
+	PDWORD GetFunctionAddressByHash(char* library, DWORD hash) const;
 
-    template<typename T>
-    T GetVTableFunction(void* pBase, int iIndex);
+	void* GetFunctionAddress(char* MyNtdllFunction, PVOID MyDLLBaseAddress) const;
 
-    static inline uintptr_t GetVirtual(void*** c, int idx)
-    {
-        return reinterpret_cast<uintptr_t>((*c)[idx]);
-    }
+	template<typename T>
+	T GetVTableFunction(void* pBase, int iIndex);
 
+	static inline uintptr_t GetVirtual(void*** c, int idx)
+	{
+		return reinterpret_cast<uintptr_t>((*c)[idx]);
+	}
 };
 
 class CPatternScan : protected CMemory
 {
 private:
-    char* ScanInWrapper(char* pattern, char* mask, char* begin, size_t size);
+	char* ScanInWrapper(char* pattern, char* mask, char* begin, size_t size);
 
-    void Parse(char* combo, char* pattern, char* mask);
+	void Parse(char* combo, char* pattern, char* mask);
 
-    char* PatternScan(char* pattern, char* mask, char* begin, size_t size);
+	char* PatternScan(char* pattern, char* mask, char* begin, size_t size);
 
-    char* TO_CHAR(wchar_t* string);
+	char* TO_CHAR(wchar_t* string);
 
-    LDR_DATA_TABLE_ENTRY* GetLDREntry(std::string name);
+	LDR_DATA_TABLE_ENTRY* GetLDREntry(std::string name);
 
-    char* ScanExWrapper(char* pattern, char* mask, char* begin, char* end, HANDLE hProc);
+	char* ScanExWrapper(char* pattern, char* mask, char* begin, char* end, HANDLE hProc);
 
 public:
-    intptr_t InPatternScan(char* combopattern, std::string szModName);
-    intptr_t InPatternScan(char* szPattern, char* szMask, std::string szModName);
-    intptr_t ExPatternScan(char* combopattern, std::string szModName, uintptr_t procID, HANDLE hProcess);
+	intptr_t InPatternScan(char* combopattern, std::string szModName);
+	intptr_t InPatternScan(char* szPattern, char* szMask, std::string szModName);
+	intptr_t ExPatternScan(char* combopattern, std::string szModName, uintptr_t procID, HANDLE hProcess);
 };
 
 class CHook : public CMemory
 {
 private:
-    BYTE m_Bytes[0x100]{ 0 };
-    void* m_oFuncAddy = nullptr;
-    size_t m_iLength = 0;
+	BYTE m_Bytes[0x100]{ 0 };
+	void* m_oFuncAddy = nullptr;
+	size_t m_iLength = 0;
 private:
-    // pOriginalFunction should be the & address of the original function we set our gateway to
-    void* Detour32(uintptr_t pHookStart, uintptr_t pOurFunction, size_t iLength);
+	// pOriginalFunction should be the & address of the original function we set our gateway to
+	void* Detour32(uintptr_t pHookStart, uintptr_t pOurFunction, size_t iLength);
 public:
-    bool Hook(void* pOriginalFunctionAddress, uintptr_t pOriginalFunction, uintptr_t ourFunction, size_t iLength);
-    bool Unhook();
+	bool Hook(void* pOriginalFunctionAddress, uintptr_t pOriginalFunction, uintptr_t ourFunction, size_t iLength);
+	bool Unhook();
 };
 
 class OffsetUpdateException : public std::exception {
 public:
-    explicit OffsetUpdateException(const std::string& message) : msg_(message) {}
-    const char* what() const noexcept override {
-        return msg_.c_str();
-    }
+	explicit OffsetUpdateException(const std::string& message) : msg_(message) {}
+	const char* what() const noexcept override {
+		return msg_.c_str();
+	}
 private:
-    std::string msg_;
+	std::string msg_;
 };
-
